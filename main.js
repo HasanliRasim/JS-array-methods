632c3ba07c0c6161dc748f37c219d489fb8dd8f2
HasanliRasim <https://github.com/HasanliRasim>
2024-10-27 17:01:18 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,95 @@
+// 1.Task: Yazı(string) uzunluğunu tapın.
+
+// let string = "Salam Dünya"
+// console.log(string.length)
+
+
+// 2.Task: Yazını böyük hərflərlə göstərin.
+
+// let string = "azərbaycan" 
+// console.log(string.toUpperCase())
+
+
+// 3.Task: Array-de müəyyən bir hissəni kəsin.
+
+// let array = [1, 2, 3, 4, 5]
+// console.log(array.slice(1,4))
+
+
+// 4.Task: Arrayin sonuna element əlavə edin.
+
+// let array =  [1, 2, 3]
+// array.push(4)
+// console.log(array)
+
+
+// 5.Task: Arrayin  sonundakı elementi silin.
+
+// let array =  [1, 2, 3]
+// array.pop(3)
+// console.log(array)
+
+
+// 6.Task: Arrayin  əvvəlindən elementi silin.
+
+// let array =  [1, 2, 3]
+// array.shift(1)
+// console.log(array)
+
+
+// 7.Task: Arrayin əvvəlinə element əlavə edin.
+
+// let array =  [1, 2, 3]
+// array.unshift(0)
+// console.log(array)
+
+
+// 8.Task:  Arrayin  müəyyən bir elementi tapın.
+
+// let array = [5, 12, 8, 130, 44]
+// let result = array.find(number => number > 10)
+// console.log(result)
+
+
+// 9.Task: Array-dəki elementləri birləşdirin.
+
+// let array = [1, 2, 3, 4]
+// console.log(array.reduce((a, b) => a + b, 0))
+
+
+// 10.Task: Array-i sıralayın.
+
+// let array = [3, 1, 4, 2] 
+// console.log(array.sort((a, b) => a - b))
+
+
+// 11. Task: Array-də müəyyən bir dəyərin olub olmadığını yoxlayın.
+
+// let array = [1, 2, 3]
+// console.log(array.includes(2))
+
+
+// 12.Task: Stringin müəyyən bir hissəsini yoxlayın.
+
+// let string = "Salam Dünya"
+// console.log(string.includes("Dünya"))
+
+
+// 13.Task: Array elementlərini birləşdirin.
+
+// let array = ["Salam", "Dünya"]
+// console.log(array.join(" "))
+
+
+// 14.Task: Arrayda filtr tətbiq edin.
+
+// let array = [1, 2, 3, 4, 5, 6, 13, 1] 
+// let answer = array.filter((value) => value % 2 == 0)
+// console.log(answer)
+
+
+// 15. Task:  array-dəki elementləri dəyişdirin.
+
+// let array = [1, 2, 3]
+// let result = array.map((value) => value * 2)
+// console.log(result)
